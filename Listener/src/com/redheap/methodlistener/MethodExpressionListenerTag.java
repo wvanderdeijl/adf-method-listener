0643d43c8ec602f8c3ca6e3fc8127d37901560a8
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-02-02 22:52:53 +0100

    Added support for JSP(X) tag

--- Listener/src/com/redheap/methodlistener/MethodExpressionListenerTag.java
+++ Listener/src/com/redheap/methodlistener/MethodExpressionListenerTag.java
@@ -1,12 +1,15 @@
 package com.redheap.methodlistener;
 
+import javax.el.ExpressionFactory;
 import javax.el.MethodExpression;
 
 import javax.faces.component.UIComponent;
 import javax.faces.view.facelets.FaceletException;
 import javax.faces.webapp.UIComponentClassicTagBase;
 
+import javax.servlet.jsp.JspApplicationContext;
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspFactory;
 
 import oracle.adf.view.rich.event.BasePolytypeListener;
 
@@ -50,7 +53,17 @@ public class MethodExpressionListenerTag extends TrinidadTagSupport {
         // create the listener and add it to the parent component
         MethodExpressionListener listener = new MethodExpressionListener(eventType);
         if (method != null) {
-            listener.setMethodExpression(method, method);
+            // TODO: cleanup
+            JspApplicationContext applicationContext =
+                JspFactory.getDefaultFactory().getJspApplicationContext(this.pageContext.getServletContext());
+            ExpressionFactory expressionFactory = applicationContext.getExpressionFactory();
+            MethodExpression noArgs =
+                expressionFactory.createMethodExpression(this.pageContext.getELContext(), method.getExpressionString(),
+                                                         null, new Class[] { });
+            MethodExpression oneArg =
+                expressionFactory.createMethodExpression(this.pageContext.getELContext(), method.getExpressionString(),
+                                                         null, new Class[] { eventType.getEventClass() });
+            listener.setMethodExpression(noArgs, oneArg);
         }
         if (!MethodExpressionListener.addListener(listener, component)) {
             throw new FaceletException("Failed to add listener for type " + eventType + " to parent " + component);
